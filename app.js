f586bab181fce8f7a8794bbd5fb67629469b8aab
dancorrea86 <danielmacielcorrea@gmail.com>
2019-12-26 18:57:35 -0300

    second

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest1');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,12 +15,17 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Make our db accessible to our router
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
